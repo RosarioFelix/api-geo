67d7b9dc35ede631fe052938a4f747d55208f7ac
Rosario Felix <elijan_11@hotmail.com>
2017-05-26 14:29:12 -0500

    subiendo implementaciones

--- js/app.js
+++ js/app.js
@@ -16,13 +16,13 @@ function buscar() {
   }
 }
 
- var latitud,longitud;
+ var latitud,longitud,miUbicacion;
  var funcionExito = function(posicion){
 
    latitud = posicion.coords.latitude;
    longitud = posicion.coords.longitude;
 
-var miUbicacion = new google.maps.Marker({ position : { lat:latitud, lng:longitud},
+ miUbicacion = new google.maps.Marker({ position : { lat:latitud, lng:longitud},
 map: map
 });
 map.setZoom(18);
@@ -32,6 +32,44 @@ map.setCenter({lat:latitud, lng:longitud});
 var funcionError = function (error){
   alert("Tenemos problemas con encontrar tu  ubicacion");
 }
-
 document.getElementById("encuentrame").addEventListener("click",buscar);
-}
+
+var inputPartida = document.getElementById('punto-partida');
+var inputDestino = document.getElementById('punto-destino');
+
+new google.maps.places.Autocomplete(inputPartida);
+new google.maps.places.Autocomplete(inputDestino);
+
+var directionsService = new google.maps.DirectionsService;
+var directionsDisplay = new google.maps.DirectionsRenderer;
+
+var calculateAndDisplayRoute = function(directionsService,directionsDisplay){
+  directionsService.route({
+    origin: inputPartida.value,
+    destination: inputDestino.value,
+    travelMode:"DRIVING"
+  }, function(response,status){
+    if (status === "OK"){
+      var distancia = Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
+      tarifa.classList.remove("none")
+      var costo = distancia*1.75;
+      if(costo<4){
+        tarifa.innerHTML = "S/.4";
+      }
+      else{
+      tarifa.innerHTML = "S/. " +  parseInt(distancia*1.75);
+    }
+      console.log(response.routes[0].legs[0].distance.text);
+      directionsDisplay.setDirections(response);
+      miUbicacion.setMap(null);
+    }else{
+      window.alert("No encontramos una ruta")
+    }
+  })
+};
+  directionsDisplay.setMap(map);
+  var trazarRuta = function(){
+    calculateAndDisplayRoute(directionsService,directionsDisplay);
+  };
+  document.getElementById("trazar-ruta").addEventListener("click", trazarRuta)
+};
